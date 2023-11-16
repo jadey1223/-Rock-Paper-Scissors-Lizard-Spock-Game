23f4cd6f528aae0a6d16e1bfce4bdd31a6ba6a55
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2023-11-16 19:10:22 +0000

    Change images names

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,28 +1,10 @@
-const game = ()=> {
-    let pscore = 0;
-    let cscore = 0;
+let you;
+let yourScore = 0;
+let computer
+let computerScore = 0;
 
-    //Play match
-    const playMatch = () => {
-        const gameArea = document.querySelectorAll(".game-area");
-        const playerhand = document.querySelectorAll("player-hand")
-        const computerhand = document.querySelectorAll("computer-hand")
+let choices = ["rock","paper","scissors","lizard","spoke"];
 
-        //Computer Options
-        const computerOptions = ["rock","paper", "sccissors", "lizard", "spock"];
-           
-        gameArea.forEach(gameArea => {
-            gameArea.addEventListener("click", function(){
-                console.log(this);
-            //Computer choice
-             const computerNumber = Math.floor(math.random()* 5);    
-            });
-        });
-
-       
-        
-    };
-};
-
-//Call all inner functions
-playMatch();
+windows.onload = function() {
+    let
+}
