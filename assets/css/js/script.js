4dc620e7deb984dcb81afcc222169216c2bff1d7
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2023-11-17 21:08:13 +0000

    fixed a bug in my javascript

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,10 +1,18 @@
-let you;
 let yourScore = 0;
 let computer
 let computerScore = 0;
 
 let choices = ["rock","paper","scissors","lizard","spoke"];
 
-windows.onload = function() {
-    let
+function loadGame() {
+    for (let i = 0; i < 5; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
 }
+
+loadGame();
