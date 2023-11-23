f2516683c8fc70b63b3a5f03c1ad90258fea1829
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2023-11-23 18:51:32 +0000

    Determine the winner in js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,3 +1,46 @@
+let result_ref = document.getElementById("result");
+let choices_objects = {
+    "rock" : {
+        "rock" : "draw",
+        "scissors": "win",
+        "paper" : "lose",
+        "lizard" : "win",
+        "spoke" : "lose"
+    },
+
+    "paper" : {
+        "rock" : "win",
+        "scissors": "lose",
+        "paper" : "draw",
+        "lizard" : "lose",
+        "spoke" : "win"
+    },
+
+    "scissor" : {
+        "rock" : "lose",
+        "scissors": "draw",
+        "paper" : "win",
+        "lizard" : "win",
+        "spoke" : "lose"
+    },
+
+    "lizard" : {
+        "rock" : "lose",
+        "scissors": "lose",
+        "paper" : "win",
+        "lizard" : "draw",
+        "spoke" : "win"
+    },
+
+    "spoke" : {
+        "rock" : "win",
+        "scissors": "win",
+        "paper" : "lose",
+        "lizard" : "lose",
+        "spoke" : "draw"
+    }
+}
+
 function checker(input){
     let choices = ["rock","paper","scissors","lizard","spoke"]
     let num = Math.floor(Math.random()*4);
@@ -6,5 +49,5 @@ function checker(input){
 
     document.getElementById("user_choice").innerHTML = `computer choose <span>${choices[num].toUpperCase()}</span>`;
 
-    
+
 }
