9709cef36a090af48aa90ca51872961165d45ba6
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2023-11-25 13:14:28 +0000

    Colors change depending on if you win ,lose or draw

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -45,20 +45,23 @@ function checker(input){
     let choices = ["rock","paper","scissor","lizard","spoke"]
     let num = Math.floor(Math.random()*5);
 
-    document.getElementById("comp_choice").innerHTML = `computer choose <span>${choices[num].toUpperCase()}</span>`;
+    document.getElementById("comp_choice").innerHTML = `Computer choose <span>${choices[num].toUpperCase()}</span>`;
 
-    document.getElementById("user_choice").innerHTML = `player choose <span>${input.toUpperCase()}</span>`;
+    document.getElementById("user_choice").innerHTML = `Player choose <span>${input.toUpperCase()}</span>`;
 
     let computer_choice = choices[num];
 
     switch(choices_object[input][computer_choice]) {
         case "win" :
+            result_ref.style.cssText = "background-color: #00cc00;";
             result_ref.innerHTML = "YOU WIN";
             break;
         case "lose" :
+            result_ref.style.cssText = "background-color: red;"
             result_ref.innerHTML = "YOU LOSE";
             break;
         default:
+            result_ref.style.cssText = "background-color: #ffff99;"
             result_ref.innerHTML = "DRAW";
             break;
     }
